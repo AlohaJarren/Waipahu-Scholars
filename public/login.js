307ba1d518784e5adcf214c0d47da88daf60fc6b
Astrocat2374 <sugiyama.evan@gmail.com>
2019-04-03 14:40:10 -1000

    Added redirect to login

--- public/login.js
+++ public/login.js
@@ -1,15 +1,17 @@
-function login() {
+let button = document.querySelector('#login-button')
+button.addEventListener('click', function login(e) {
+  e.preventDefault();
 
-
-
-    let email = document.querySelector('#email').value
-    let password = document.querySelector('#password').value
-    console.log(email)
-    console.log(password)
-    fire
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch(err => {
-        console.log(err.message);
-      });
-  }
+  let email = document.querySelector('#email').value
+  let password = document.querySelector('#password').value
+  console.log(email)
+  console.log(password)
+  localStorage.setItem('user', email)
+  window.location = 'http://localhost:8080/public/goals.html'
+  fire
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(err => {
+      console.log(err.message);
+    });
+})
