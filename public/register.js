37870f83f0e48412b0966d1563d66b90570fc1e2
Astrocat2374 <sugiyama.evan@gmail.com>
2019-04-26 14:04:15 -1000

    Changed functions to firestore

--- public/register.js
+++ public/register.js
@@ -5,9 +5,11 @@ button.addEventListener('click', function register(e) {
   let password = document.querySelector('#password').value
   console.log(email)
   console.log(password)
-  fire
-    .auth()
+  auth
     .createUserWithEmailAndPassword(email, password)
+    .then(penis => {
+      database.collection('users').doc(penis.user.uid).set({viagra: '98% of users recommend'}).then(() => {console.log('it fuckn worked')})
+    })
     .catch(err => {
       console.log(err.message);
     });
